a665e0e561af01b24b58f316358c256bb39d3b0d
Sergey_A <ananinsergey94@gmail.com>
2018-02-15 16:34:18 +0500

    фавикон, форма теперь появляется а все испаряется пухфух

---
+++ app/form.php
@@ -0,0 +1,30 @@
+<?php 
+ 
+$sendto   = "dalbaprosto@gmail.com"; // почта, на которую будет приходить письмо
+$username = $_POST['name'];   // сохраняем в переменную данные полученные из поля c именем
+$usermail = $_POST['email']; // сохраняем в переменную данные полученные из поля c адресом электронной почты
+$theme = $_POST['theme']; //тема
+$message = $_POST['message']; // сообщение
+
+// Формирование заголовка письма
+$subject  = . strip_tags($theme) .;
+$headers  = "From: " . strip_tags($usermail) . "\r\n";
+$headers .= "Reply-To: ". strip_tags($usermail) . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html;charset=utf-8 \r\n";
+ 
+// Формирование тела письма
+$msg  = "<html><body style='font-family:Arial,sans-serif;'>";
+$msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Cообщение с сайта</h2>\r\n";
+$msg .= "<p><strong>От кого:</strong> ".$username."</p>\r\n";
+$msg .= "<p><strong>Почта:</strong> ".$usermail."</p>\r\n";
+$msg .= "</body></html>";
+ 
+// отправка сообщения
+if(@mail($sendto, $subject, $msg, $headers)) {
+    echo "<center>OK</center>";
+} else {
+    echo "<center>ERROR</center>";
+}
+ 
+?>
--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,27 @@
 $(function(){
 	new WOW().init();
+
+	var button = $('#bugReport');
+	var form = $('#form');
+	var menu = $('#menu');
+
+	var hiddenContent = 'hidden-content';
+
+	button.bind('click', toggleForm);
+	
+	function toggleForm(){
+		console.log(form, menu);
+		form.toggleClass(hiddenContent);
+		menu.toggleClass(hiddenContent);
+	}
 });
+
+/*function openbox(id){
+    display = document.getElementById(id).style.display;
+    if(display=='block'){
+       document.getElementById(id).style.display='none';
+    }else{
+       document.getElementById(id).style.display='block';
+    }
+}
+*/
